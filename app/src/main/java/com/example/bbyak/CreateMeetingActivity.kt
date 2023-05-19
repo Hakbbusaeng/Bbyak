286d79749214b3633ce374277dd06d01a5ce14ab
lynnily <lynnilyh@gmail.com>
2023-05-20 04:13:54 +0900

    Feat: 뺙 생성하기

--- app/src/main/java/com/example/bbyak/CreateMeetingActivity.kt
+++ app/src/main/java/com/example/bbyak/CreateMeetingActivity.kt
@@ -2,11 +2,15 @@ package com.example.bbyak
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.UserHandle
 import android.util.Log
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
 import com.example.bbyak.databinding.ActivityCreateMeetingBinding
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import java.util.*
+import kotlin.collections.ArrayList
 
 class CreateMeetingActivity : AppCompatActivity() {
 
@@ -18,6 +22,9 @@ class CreateMeetingActivity : AppCompatActivity() {
     private lateinit var fcmFragment: FinishCreateMeetingFragment
 
     private lateinit var meetingCode: String
+
+    private val database = Firebase.database
+    private val meeting = database.getReference("meeting")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -56,16 +63,39 @@ class CreateMeetingActivity : AppCompatActivity() {
         }
     }
 
+    data class Date(val year: Int, val month: Int, val day: Int)
+    data class User(val id: String, val date: List<Date>, val time:List<Boolean>)
+    data class Meeting(val id: String, val name: String, val date: List<Date>, val user: List<User>?)   //user: 나중에 수정하기
+
     private fun createMeeting() {
-        Log.e("meeting name", cmFragment.binding.editTextMeetingName.text.toString())
+        // Meeting Id
+        val key = meeting.push().key.toString()
+        Log.e("meeting key", key)
+
+        // Meeting Name
+        val name = cmFragment.binding.editTextMeetingName.text.toString()
+        Log.e("meeting name", name)
+
+        // Meeting Date
+        val date = ArrayList<Date>()
         for (i in cmFragment.binding.selectCalendarView.selectedDates) {
+            val year = i.get(Calendar.YEAR)
+            val month = i.get(Calendar.MONTH) + 1
+            val day = i.get(Calendar.DAY_OF_MONTH)
+            val ymd = Date(year, month, day)
+
             Log.e(
-                "selected date",
-                "${i.get(Calendar.YEAR)}/${i.get(Calendar.MONTH) + 1}/${i.get(Calendar.DAY_OF_MONTH)}"
+                "selected date", year.toString() + "/" + month.toString() + "/"+ day.toString()
             )
+
+            date.add(ymd)
         }
 
+        // Add Meeting
+        val newMeeting = Meeting(key, name, date, null)
+        meeting.child(key).setValue(newMeeting)
+
         //get code
-        meetingCode = "A36G9E"
+        meetingCode = key
     }
 }
