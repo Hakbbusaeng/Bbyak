89d44eac2572bd9b889831b4822327af86ea9156
lynnily <lynnilyh@gmail.com>
2023-05-28 18:14:02 +0900

    Refactor: schedule 변수 형태 수정

--- app/src/main/java/com/example/bbyak/getData.kt
+++ app/src/main/java/com/example/bbyak/getData.kt
@@ -75,7 +75,7 @@ fun toSelectedTime(): ArrayList<Pair<Int, Int>> {
 
     val uSch = getUserSchedule()
     //println("uSch: $uSch")
-    val uSchInt = uSch.map { it.split(",").map { num -> num.toInt() }}
+    val uSchInt = uSch.map { it.map { digit -> digit.toString().toInt() } }
     //println("uSchInt: $uSchInt")
 
     var day = 1
@@ -99,7 +99,7 @@ fun toUserSchedule(selectedTime: ArrayList<Pair<Int, Int>>): ArrayList<String> {
 
     val schedule = ArrayList<String>()
     for (i in arrSelectedTime) {
-        schedule.add(i.joinToString(",") { it.toString() })
+        schedule.add(i.joinToString("") { it.toString() })
     }
 
     return schedule
